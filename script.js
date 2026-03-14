512f0190ecc4e350328b2f95d19f07944a8c9c6c
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-14 07:54:29 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const btn = document.getElementById('btn')
 const ul = document.getElementById('users')
+const showError = document.getElementById('error')
+
+const loading = document.getElementById('loading')
 
 // btn.addEventListener('click',()=>{
 //     ul.innerHTML = ''
@@ -18,16 +21,44 @@ const ul = document.getElementById('users')
    
 // })
 
-btn.addEventListener('click',async()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+// btn.addEventListener('click',async()=>{
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-    const data = await response.json()
-    data.forEach(user=>{
-        const li = document.createElement('li')
-        li.textContent = user.name
+//     const data = await response.json()
+//     data.forEach(user=>{
+//         const li = document.createElement('li')
+//         li.textContent = user.name
 
-        ul.appendChild(li)
-    })
+//         ul.appendChild(li)
+//     })
+// })
+
+btn.addEventListener('click', async()=>{
+    ul.innerHTML = ''
+    showError.textContent = ''
+    loading.textContent = 'Loading..'
+
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+
+        loading.textContent = ''
+
+        data.forEach(user =>{
+            const li = document.createElement('li')
+
+            li.textContent = user.name
+
+            ul.appendChild(li)
+        })
+
+
+    } catch (error) {
+        loading.textContent = ''
+        showError.innerText = 'Unable to load'
+    }
+
 })
     
  
