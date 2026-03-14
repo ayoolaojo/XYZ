8bb4d49a304961a5402bbce68114911e000ba533
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-14 07:24:31 +0100

    updated files

--- script.js
+++ script.js
@@ -2,24 +2,35 @@
 const btn = document.getElementById('btn')
 const ul = document.getElementById('users')
 
-btn.addEventListener('click',()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
+// btn.addEventListener('click',()=>{
+//     ul.innerHTML = ''
+//     fetch('https://jsonplaceholder.typicode.com/posts')
 
-    .then(response => response.json())
-    .then(data => data.forEach(user=>{
+//     .then((response)=>response.json())
+//     .then(data=>{
+//         data.forEach(post=>{
+//             const li = document.createElement('li')
+
+//             li.textContent = `${post.id  }    - ${post.title}`i
+//             ul.appendChild(li)
+//         })
+//     })
+   
+// })
+
+btn.addEventListener('click',async()=>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await response.json()
+    data.forEach(user=>{
         const li = document.createElement('li')
         li.textContent = user.name
 
-      ul.appendChild(li)
-    }))
-
-   
-   
-
-
-        
+        ul.appendChild(li)
+    })
 })
-
+    
+ 
 
 
 
